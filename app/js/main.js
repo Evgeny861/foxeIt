d4d6b1e32532ba89ce7f8339519ae63eb4b6b3ac
Evgeny Tunnikov <tunnikov2013@yandex.ru>
2020-10-03 23:16:49 +0300

    fonts

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,13 @@
 import carousel from './carousel.js';
-import tree from './tree.js';
 import videoPlayerInit from './videoPlayerInit.js';
 import services from './services.js';
 import problem from './problem.js';
-import team from './team.js'
+import team from './team.js';
+import scroll from './scroll.js';
 
 carousel();
-tree();
 videoPlayerInit();
 services();
 problem();
 team();
+scroll();
